e413109ec9dfcb569285444c8aa3bf84877b0448
Maxime Doyen Blec <89549493+mdoyenblec@users.noreply.github.com>
2024-03-27 08:03:25 +0100

    Delete offers directory

--- offers/job_board/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import JobOffer
-
-class JobOfferForm(forms.ModelForm):
-    class Meta:
-        model = JobOffer
-        fields = ['number','title', 'company', 'application_date','url']
-        
