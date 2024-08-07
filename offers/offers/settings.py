8c273582724c2cfbf77b1f356aab438abe3e4c8a
Maxime Doyen Blec <89549493+mdoyenblec@users.noreply.github.com>
2024-08-07 14:52:19 +0200

    Update settings.py

--- offers/offers/settings.py
+++ offers/offers/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-!9ro@%y(kk8c&h-yo-#5pi@w&l_t0y$&l-bg4vkm0lnbu44+0w"
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
